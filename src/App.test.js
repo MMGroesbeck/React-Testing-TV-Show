f4751a3693934cadf8ad3f9b7b04a0432431e0fd
M Groesbeck <m.groesbeck@gmail.com>
2020-03-27 06:13:14 -0700

    Async test of loading passes.

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,18 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 import App from "./App";
+
+import { fetchShow as mockFetchShow } from "./api/fetchShow";
+import { testData } from "./testData";
+
+jest.mock("./api/fetchShow");
+
+test ("Stranger Things main page loads", async () => {
+    mockFetchShow.mockResolvedValueOnce(testData);
+    const { getByText, queryAllByTestId } = render (<App/>);
+    await waitFor(() => {
+        expect(mockFetchShow).toHaveBeenCalledTimes(1);
+    })
+});
